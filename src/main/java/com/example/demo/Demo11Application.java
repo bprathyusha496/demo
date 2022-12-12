77ed8489b6ca50aaa7acc4c698b1eaa13b57aaea
bprathyusha496 <113101441+bprathyusha496@users.noreply.github.com>
2022-12-12 15:27:16 +0530

    added spaces

--- src/main/java/com/example/demo/Demo11Application.java
+++ src/main/java/com/example/demo/Demo11Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo11Application {
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Demo11Application.class, args);
 	}
